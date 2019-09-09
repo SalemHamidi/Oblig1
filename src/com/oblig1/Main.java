b2d79d9193dfce0a7f3d17e40af6f484fcb0ecc8
SalemHamidi <43269413+SalemHamidi@users.noreply.github.com>
2019-09-09 22:14:01 +0200

    Test

---
+++ src/com/oblig1/Main.java
@@ -0,0 +1,9 @@
+package com.oblig1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Test: Intellij - Github");
+    }
+}
